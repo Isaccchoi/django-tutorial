964e85cf6132a9a1e0fee4d84f4b97d2e530ad9a
isaccchoi <isaccchoi@naver.com>
2017-09-29 14:10:43 +0900

    change index view, create question_detail view

--- tutorial/config/urls.py
+++ tutorial/config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from polls.views import index
+from polls.views import index, question_detail
+from .views import static_file
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
+    url(r'^polls/(?P<pk>\d+)/$', question_detail, name='question_detail')
 ]
--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -2,5 +2,25 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from django.shortcuts import render
+
+from .models import Question, Choice
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    questions = Question.objects.all()
+    context = {
+        'questions': questions
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def question_detail(request, pk):
+    question = Question.objects.get(pk=pk)
+    choices = Choice.objects.filter(question=question)
+    context = {
+        'question': question,
+        'choices': choices,
+    }
+
+    return render(request, 'polls/question.html', context)
